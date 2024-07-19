e834902c4ef0c2d47b1bf3382a87e900ee86633f
jakob <jakobsereda@gmail.com>
2024-07-19 13:41:10 -0600

    feat: changed keys and added comment for .env variables

--- src/create-vc.js
+++ src/create-vc.js
@@ -1,6 +1,13 @@
 const jose = require('jose')
 require('dotenv').config()
 
+/**
+ * ENVIRONMENT VARIABLES:
+ * ======================
+ *   - PEM_PRIVATE_KEY: private key in .pem format
+ *   - PEM_PUBLIC_KEY: public key in .pem format
+ */
+
 console.log(process.env.PEM_PRIVATE_KEY);
 console.log(process.env.PEM_PUBLIC_KEY);
 
